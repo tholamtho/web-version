dbbcac1620cee2f974486bbf4bce86a1218996c1
tholamtho <lamtho1999@gmail.com>
2022-06-17 12:30:13 +0700

    commit web app

--- src/App.tsx
+++ src/App.tsx
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Food Order App
         </a>
       </header>
     </div>
